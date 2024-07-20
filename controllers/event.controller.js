7a8eba6a4c3ea3f5d1b3fd3421699185c33a5692
Steven Patino <stevenwork75@gmail.com>
2024-07-19 21:19:06 -0500

    agregando event controller

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -2,4 +2,31 @@ const Event = require('./../models/Event')
 
 const createEvent = async( req, res) => {
     const {name, date, hour, location, cost, distance, capacity, category} = req.body
+
+    try{
+        const dbEvent = new Event({
+            name: name,
+            date: date, 
+            hour: hour, 
+            location: location, 
+            cost: cost, 
+            distance: distance,
+            capacity: capacity, 
+            category: category
+        })
+        await dbEvent.save(); 
+        return res.status(201).json({
+            ok: true, 
+            msg: 'Evento creado exitosamente'
+        })
+
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: 'Server error unable to create event, contact support'
+        })
+    }
 }
+
+module.exports = createEvent; 
