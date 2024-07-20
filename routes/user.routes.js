297e849fbab9045172b8f3c4c407b9c0726a945f
Steven Patino <stevenwork75@gmail.com>
2024-07-19 21:34:58 -0500

    arreglo user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const {createUser, loginUser, updateUser, deleteUser} = require('./../controllers/user.controller')
-const users = require('./../middlewares/validationBody')
+const {users, events} = require('./../middlewares/validationBody')
 const validateFields = require('./../middlewares/validationResult')
 
 
-router.post('/registro', users, validateFields, createUser)
 router.post('/login', validateFields, loginUser)
 router.put('/update-user', validateFields, updateUser)
 router.delete('/delete-user/', deleteUser)
