a500f164609046e34af95cf8ad31df9914ed7219
Steven Patino <stevenwork75@gmail.com>
2024-08-13 20:46:26 -0500

    create 2 new controllers and their routes

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -47,6 +47,77 @@ const getAllEvents = async( req, res) => {
     }
 }
 
+const getEventById = async(req, res) => {
+    const id = req.params.id
+    try{
+        const event = await Event.findById(id); 
+        if(!event){
+            return res.status(404).json({
+                ok: false,
+                msg:'Event Not Found'
+            })
+        }
+        return res.status(202).json({
+            ok: true, 
+            msg: 'File found',
+            event: event
+        })
+
+    } catch(error){
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: 'Could not get event, please try again'
+        })
+    }
+}
+
+const updateEventById = async(req, res) =>{
+
+    const id = req.params.id
+
+    const name = req.body.name; 
+    const date = req.body.date;
+    const hour = req.body.hour; 
+    const location = req.body.location; 
+    const cost = req.body.cost; 
+    const distance = req.body.distance; 
+    const capacity = req.body.capacity; 
+    const category = req.body.category; 
+
+    try{
+        const updatedData = {}
+
+        if(name)updatedData.name = name; 
+        if(date)updatedData.date = date; 
+        if(hour)updatedData.hour = hour; 
+        if(location)updatedData.location = location; 
+        if(cost)updatedData.cost = cost; 
+        if(distance)updatedData.distance = distance; 
+        if(capacity)updatedData.capacity = capacity; 
+        if(category)updatedData.category = category; 
+
+        const event = await Event.findByIdAndUpdate(id, updatedData)
+
+        if(!event) return res.status(404).json({
+            ok: false,
+            msg: 'Event not found for update'
+        })
+        return res.status(200).json({
+            ok:  true,
+            msg: 'Event updated successfuly',
+            event: event
+        })
+
+    } catch(error){
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: 'server error updating the vent, contact support'
+        })
+    }
+}
+
 const deleteEventById = async(req, res) => {
     const {id} = req.params.id
     try{
@@ -74,5 +145,7 @@ const deleteEventById = async(req, res) => {
 module.exports = {
     createEvent,
     deleteEventById,
-    getAllEvents
+    getAllEvents,
+    getEventById,
+    updateEventById
 } 
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {createEvent, deleteEventById, getAllEvents} = require('./../controllers/event.controller')
-const {users, events} = require('./../middlewares/validationBody')
+const {createEvent, deleteEventById, getAllEvents, getEventById, updateEventById} = require('./../controllers/event.controller')
+const {events} = require('./../middlewares/validationBody')
 const validateFields = require('./../middlewares/validationResult')
 
 
 router.post('/reg-event', events, validateFields, createEvent)
 router.get('/get-all', getAllEvents)
+router.get('/get-event/:id', getEventById)
+router.put( '/update-event/:id', updateEventById)
 router.delete('/del-event/:id', deleteEventById)
 
 module.exports = router
